92361997633a9306c3d1aee714aa7c4fc07acc03
Russell <russellwmy@gmail.com>
2019-07-28 02:11:32 +0800

    refactor: added readonly modifier to properties

--- src/lib/Block.ts
+++ src/lib/Block.ts
@@ -1,12 +1,11 @@
 import Transaction from './Transaction';
 
 interface Block {
-  transactions: Array<Transaction>; // Array of transactions
-  previousHash?: string;            // Hexadecimal hash of a previous Block in the chain
-
-  timestamp?: number;               // Unix time when Block was mined
-  nonce: number;                    // Cryptographic nonce number
-  difficulty: number;               // Number representing the mining difficulty
+  readonly transactions: Array<Transaction>; // Array of transactions
+  readonly previousHash?: string;            // Hexadecimal hash of a previous Block in the chain
+  readonly timestamp?: number;               // Unix time when Block was mined
+   nonce: number;                    // Cryptographic nonce number
+  readonly difficulty: number;               // Number representing the mining difficulty
 }
 
 export default Block;
--- src/lib/Transaction.ts
+++ src/lib/Transaction.ts
@@ -1,9 +1,9 @@
 interface Transaction {
-  sender: string;     // Public key address of a transaction sender 
-  recipient: string;  // Public key address of a transaction recipient
-  amount: number;     // Amount of coins to be transferred
-  timestamp: number;  // Unix time when transaction was created
-  coinbase: boolean;  // True if it's a coinbase transaction ie. transaction that awards the miner and creates new coins
+  readonly sender: string;     // Public key address of a transaction sender 
+  readonly recipient: string;  // Public key address of a transaction recipient
+  readonly amount: number;     // Amount of coins to be transferred
+  readonly timestamp: number;  // Unix time when transaction was created
+  readonly coinbase: boolean;  // True if it's a coinbase transaction ie. transaction that awards the miner and creates new coins
 
   signature?: string; // Transaction hash encrypted with the private key of the sender
 }
